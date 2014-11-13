939436b86aa0ad881653a22b3014183d95abb2c1
Nicola <Martin@Martins-MacBook-Air-2.local>
2014-11-13 15:25:41 +0000

    Revisions to Map Page

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -9,18 +9,23 @@ class PubsController < ApplicationController
 	end
 
 	def create
+
 		@pub = Pub.new(pub_params)
+		
 		if @pub.save
-				flash[:success] = "Thanks for adding a new Pub"
-				redirect_to pubs_path
-			else
-				flash[:error] = "Oops, Sorry, something went wrong please try again"
-				render :new
-			end
+			redirect _to root_path
+		else
+			flash[:error] = "Oops, Sorry, something went wrong we did not find that Pub please try again" if address_err?
+			render :new
+		end
 	end
 
 private
 	def pub_params
 		params.require(:pub).permit(:name, :address, :latitude, :longitude, :rating)
+	end
+
+	def address_err?
+	 @pub.errors.full_messages == ["Latitude can't be Blank", "Longitude can be blank"]
 	end
 end
--- app/models/pub.rb
+++ app/models/pub.rb
@@ -1,2 +1,10 @@
 class Pub < ActiveRecord::Base
+	validates :name, presence: true, uniqueness: { scope: :address }
+	validates :address, presence: true, uniqueness: { scope: :name }
+	validates :latitude, presence: true
+	validates :longitude, presence: true
+	
+	geocoded_by :address
+	before_validation :geocode
+
 end
