2dc54b9eb033d4d5c397c7b42217a0791f27f1ec
Nicola <Martin@Martins-MacBook-Air-2.local>
2014-11-13 16:30:06 +0000

    Revised Code to correct Error with Search

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -1,7 +1,17 @@
 class PubsController < ApplicationController
 	
 	def index
-		@pubs = Pub.all
+		if  pubs_near?
+			flash.now[:success] = "Showing Pubs Near #{params[:search][:q]}"
+			@pubs = Pub.near(params[:search][:q])
+
+		elsif query?
+			flash.now[:error] = "Oops, Sorry, something went wrong we did not find #{params[:search][:q]} please try again"
+			@pubs = Pub.all
+		
+		else
+			@pubs = Pub.all
+		end
 	end
 
 	def new
@@ -9,23 +19,38 @@ class PubsController < ApplicationController
 	end
 
 	def create
-
 		@pub = Pub.new(pub_params)
 		
 		if @pub.save
-			redirect _to root_path
+			redirect_to root_path
 		else
-			flash[:error] = "Oops, Sorry, something went wrong we did not find that Pub please try again" if address_err?
-			render :new
+			@pubs = Pub.all
 		end
 	end
 
 private
-	def pub_params
-		params.require(:pub).permit(:name, :address, :latitude, :longitude, :rating)
-	end
+		def pub_params
+			params.require(:pub).permit(:name, :address, :latitude, :longitude, :rating)
+		end
 
-	def address_err?
-	 @pub.errors.full_messages == ["Latitude can't be Blank", "Longitude can be blank"]
-	end
+		def address_err?
+		 	@pub.errors.full_messages == ["Latitude can't be Blank", "Longitude can be blank"]
+		end
+		
+		def search?
+			params[:search].present?
+		end
+
+		def query?
+			if search?
+			params[:search][:q].present?
+			end
+		end
+
+		def pubs_near?
+			if search? and query?
+		 	Pub.near(params[:search][:q]).any?
+		 	end
+		end
+
 end
